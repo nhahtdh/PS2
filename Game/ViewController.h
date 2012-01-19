a47487cb76da479b9984de9adfa4fdc539e6114e
Hong Dai Thanh <hdthanh@soctf-imac-003.comp.nus.edu.sg>
2012-01-19 18:54:43 +0800

    Sample interface.

--- Game/ViewController.h
+++ Game/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)buttonPressed:(id)sender;
+@property (weak, nonatomic) IBOutlet UIScrollView *gamearea;
 
 @end
--- Game/ViewController.m
+++ Game/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize gamearea;
 
 - (void)didReceiveMemoryWarning
 {
@@ -21,11 +22,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Load the images into UIImage objects
+    UIImage *bgImage = [UIImage imageNamed:@"background.png"];
+    UIImage *groundImage = [UIImage imageNamed:@"ground.png"];
+    
+    // Get the width and height of the two images
+    CGFloat backgroundWidth = bgImage.size.width;
+    CGFloat backgroundHeight = bgImage.size.height;
+    CGFloat groundWidth = groundImage.size.width;
+    CGFloat groundHeight = groundImage.size.height;
+    
+    // Place each of them in an UIImageView
+    UIImageView *background = [[UIImageView alloc] initWithImage:bgImage];
+    UIImageView *ground = [[UIImageView alloc] initWithImage:groundImage];
+    CGFloat groundY = gamearea.frame.size.height - groundHeight;
+    CGFloat backgroundY = groundY - backgroundHeight;
+    
+    // The frame property holds the position and size of the views
+    // the CGRectMake methods arguments are : x position, y position, width,
+    // height
+    background.frame = CGRectMake(0, backgroundY, backgroundWidth, backgroundHeight);
+    ground.frame = CGRectMake(0, groundY, groundWidth, groundHeight);
+    
+    // Add these views as subviews of the gamearea.
+    [gamearea addSubview:background];
+    [gamearea addSubview:ground];
+    
+    // Set the content size so that gamearea is scrollable
+    // otherwise it defaults to the current window size
+    
+    CGFloat gameareaHeight = backgroundHeight + groundHeight;
+    CGFloat gameareaWidth = backgroundWidth;
+    [gamearea setContentSize:CGSizeMake(gameareaWidth, gameareaHeight)];
 }
 
 - (void)viewDidUnload
 {
+    [self setGamearea:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -61,4 +96,15 @@
     }
 }
 
+- (IBAction)buttonPressed:(id)sender {
+    UIColor *newColor;
+    UIButton *button = (UIButton*)sender;
+    if ([button titleColorForState:UIControlStateNormal] ==
+        [UIColor blackColor]) {
+        newColor = [UIColor lightGrayColor];
+    } else {
+        newColor = [UIColor blackColor];
+    }
+    [button setTitleColor:newColor forState:UIControlStateNormal];
+}
 @end
